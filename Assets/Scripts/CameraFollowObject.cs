96e94c3c88e7b81a891a02cacd444bcb4fdfcd65
Lozic71 <luizhenriquegm71@gmail.com>
2023-03-27 17:11:33 -0300

    Look up and Down movement

--- Assets/Scripts/CameraFollowObject.cs
+++ Assets/Scripts/CameraFollowObject.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
@@ -25,6 +26,16 @@ public class CameraFollowObject : MonoBehaviour
 
     public bool FollowPlayer;
 
+    [Header("Look up and Down")]
+    public float heightAmount = 0f;
+    public bool LookedUpOrDown = false;
+    public float LerpTimeDurationUpAndDown = 3f;
+    public float LerpElapsedTimeUpAndDown = 0f;
+    [SerializeField]
+    private AnimationCurve curveUpAndDown;
+
+    private float currentY;
+
     private void Awake()
     {
         PlayerCharacterController = PlayerTransform.GetComponent<CharacterController2D>();
@@ -39,9 +50,27 @@ public class CameraFollowObject : MonoBehaviour
         if(FollowPlayer)
         {
             transform.position = PlayerTransform.position;
+            currentY = transform.position.y;
         }
 
+        if (Input.GetAxis("Vertical") >0f  && PlayerCharacterController.m_Grounded && LerpElapsedTimeUpAndDown < LerpTimeDurationUpAndDown)
+        {
+            LerpUp(currentY);
+        }
+        if (Input.GetAxis("Vertical") < 0f  && PlayerCharacterController.m_Grounded && LerpElapsedTimeUpAndDown < LerpTimeDurationUpAndDown)
+        {
+            LerpDown(currentY);
+        }
+        if (Input.GetAxis("Vertical") == 0 && LookedUpOrDown)
+        {
+            
+            Lerp();
+            
+        }
     }
+
+
+
     public void CallTurn()
     {
         //smooth when falling
@@ -63,12 +92,43 @@ public class CameraFollowObject : MonoBehaviour
             return 180f;
         }
     }
+    public void LerpUp(float currentY) 
+    {
+        LerpElapsedTime = 0f;
+        FollowPlayer = false;
+        LerpElapsedTimeUpAndDown += Time.deltaTime;
+
+        float percentageComplete = LerpElapsedTimeUpAndDown / LerpTimeDurationUpAndDown;
+
+        float Up = Mathf.Lerp(transform.position.y,(currentY + heightAmount), curve.Evaluate(percentageComplete));
+
+        transform.position = new Vector3(transform.position.x, Up);
+
+        LookedUpOrDown = true;
+
+    }
+    public void LerpDown(float currentY)
+    {
+        LerpElapsedTime = 0f;
+        FollowPlayer = false;
+        LerpElapsedTimeUpAndDown += Time.deltaTime;
+
+        float percentageComplete = LerpElapsedTimeUpAndDown / LerpTimeDurationUpAndDown;
+
+        float Up = Mathf.Lerp(transform.position.y, (currentY - heightAmount), curve.Evaluate(percentageComplete));
+
+        transform.position = new Vector3(transform.position.x, Up);
+
+        LookedUpOrDown = true;
+
+    }
 
     //method to make the object follow player after leaving a no move area
     public void Lerp()
     {
-        
-        LerpElapsedTime += Time.deltaTime;
+
+        LerpElapsedTimeUpAndDown = 0;
+       LerpElapsedTime += Time.deltaTime;
 
         float percentageComplete = LerpElapsedTime / DesireLerpDuration;
 
@@ -77,6 +137,7 @@ public class CameraFollowObject : MonoBehaviour
         if (LerpElapsedTime >= 5f)
         {
             FollowPlayer = true;
+            LookedUpOrDown = false;
         }
         
     }
