bd9f5ed5f02e6cf60eaec384232fcb202be6e29f
Rodrigo Mata <rodrigomataspengler@gmail.com>
2023-03-29 13:57:28 -0300

    Puzzle sequencia feito backend

---
+++ Assets/Puzzle/Puzzle sequancia/PuzzleSequanciaMain.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleSequanciaMain : MonoBehaviour
+{
+    [SerializeField] private GameObject[] blocos;
+    [SerializeReference]private bool puzzleFeito = false;
+
+    private void Update()
+    {
+        if (Verificar())
+        {
+            puzzleFeito = true;
+            FeedBack();
+        }
+    }
+
+    private bool Verificar()
+    {
+        for (int i = 0; i < blocos.Length; i++)
+        {
+            if (blocos[i].GetComponent<PuzzleSequencioa>().state == false)
+            {
+                return false;
+                break;
+            }
+
+        }
+        return true;
+    }
+
+    private void FeedBack()
+    {
+        for (int i = 0; i < blocos.Length; i++)
+        {
+            blocos[i].GetComponent<SpriteRenderer>().color = Color.green;
+            blocos[i].GetComponent<PuzzleSequencioa>().enabled = false;
+
+        }
+    }
+}
