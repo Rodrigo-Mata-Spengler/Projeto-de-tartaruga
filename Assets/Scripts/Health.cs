bc7677a53a1e7f42c9a7470b1766c8056455f343
Lozic71 <luizhenriquegm71@gmail.com>
2023-03-30 22:30:51 -0300

    inimigo

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,30 +5,44 @@ using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-    public float MaxHealth; // the maximum of life
+    //public float MaxHealth; // the maximum of life
 
     [SerializeField] private Slider HealthSlider;
-
-    [Header("Current health")]
-    public float CurrentHealth; // current life
+    //public float CurrentHealth; // current life
+    public int lifeBar;
 
 
     private void Start()
     {
-        CurrentHealth = MaxHealth;
+        //CurrentHealth = MaxHealth;
+
+
     }
 
-
+    private void Update()
+    {
+        if(lifeBar <= 0f)
+        {
+            Debug.Log("morreu");
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            Damage(-1);
+        }
+    }
 
     // Do damage
-    public void Damage(float GiveDamageAmount)
+    public void Damage(int GiveDamageAmount)
     {
-        CurrentHealth -= GiveDamageAmount;
+        lifeBar -= GiveDamageAmount;
     }
 
     //give heath
-    public void GiveHealth(float GiveHealthAmount)
+    public void GiveHealth(int GiveHealthAmount)
     {
-        CurrentHealth += GiveHealthAmount;
+        lifeBar += GiveHealthAmount;
     }
 }
