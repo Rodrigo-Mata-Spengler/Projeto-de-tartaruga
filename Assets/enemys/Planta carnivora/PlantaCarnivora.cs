3dcdb7ad0fd965c74a434694dc2751a399232de8
Rodrigo Mata <rodrigomataspengler@gmail.com>
2023-04-12 09:51:02 -0300

    update planta

--- Assets/enemys/Planta carnivora/PlantaCarnivora.cs
+++ Assets/enemys/Planta carnivora/PlantaCarnivora.cs
@@ -2,16 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-enum StatusPlanta { dormindo, atacando, dandoInpulso};
+enum StatusPlanta { dormindo, atacando, esperando};
 public class PlantaCarnivora : MonoBehaviour
 {
     [Header("Status Planta")]
     [SerializeField] private StatusPlanta status;
+
     [Header("detecção player")]
     [SerializeField] private GameObject player;
     [SerializeField] private string playerTag;
     [SerializeField] private float areaDeteccao = 0;
     private CircleCollider2D colisorDeteccao;
+
+    [Header("Ataque")]
+    [SerializeField] private float distanciaMaximaAtaque = 0;
+    [SerializeField] private float tempoEsperaAtaque = 0;
+    private float tempoEsperaAtaqueProximo = 0;
+    [SerializeField] private float velocidadeAtaque = 0;
+    private float velocidadeAtaquetempo = 0;
+    [SerializeField] private float quantidadeAtaques = 0;
+    private float quantidadeAtaqueAtual = 0;
 
     private void Start()
     {
@@ -28,7 +38,8 @@ public class PlantaCarnivora : MonoBehaviour
                 break;
             case StatusPlanta.atacando:
                 break;
-            case StatusPlanta.dandoInpulso:
+            case StatusPlanta.esperando:
+                EsperaAtaque();
                 break;
         }
     }
@@ -41,14 +52,28 @@ public class PlantaCarnivora : MonoBehaviour
             player = collision.gameObject;
         }
     }
+
     //detectando o player ela entra em modo de ataque 
     private void IfPlayer()
     {
         if (player)
         {
+            status = StatusPlanta.esperando;
+            tempoEsperaAtaqueProximo = Time.time + tempoEsperaAtaque;
+        }
+    }
+
+    //Espera um tempo até atacar de novo
+    private void EsperaAtaque()
+    {
+        if (tempoEsperaAtaqueProximo < Time.time)
+        {
             status = StatusPlanta.atacando;
         }
     }
+
     //em modo te ataque ela tenta atacar o player varias vezez consecutivas
+
+
     //depois desses ataques ela volta a dormir esperando detectar o player de novo
 }
