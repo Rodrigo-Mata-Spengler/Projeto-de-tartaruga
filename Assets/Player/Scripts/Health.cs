d593b603dc6f20977400064f1bccfaaa0fbab23f
Lozic71 <luizhenriquegm71@gmail.com>
2023-04-15 23:52:43 -0300

    seaweed heal not done

--- Assets/Player/Scripts/Health.cs
+++ Assets/Player/Scripts/Health.cs
@@ -3,21 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Cinemachine;
+using Unity.VisualScripting;
 
 public class Health : MonoBehaviour
 {
     private Rigidbody2D rb;
-    //public float CurrentHealth; // current life
-    public int lifeBar;
 
-    //public float MaxHealth; // the maximum of life
+    public int maxLife;
+    public int currentLife;
+    public int HealSeaweed;
+
+    public float CurrenTime = 0f;
+    public float TimeToHeal = 1.5f;
     [SerializeField] private GameObject[]lifeImages;
 
-
     private CinemachineImpulseSource impulseSource;
+
+    public bool DoDamage = false;
     private void Start()
     {
-        //CurrentHealth = MaxHealth;
+        currentLife = maxLife;
 
         rb = gameObject.GetComponent<Rigidbody2D>();
 
@@ -26,7 +31,13 @@ public class Health : MonoBehaviour
 
     private void Update()
     {
-        if(lifeBar <= 0f)
+        if (DoDamage)
+        {
+            Damage(1);
+            DoDamage= false;
+        }
+
+        if(currentLife <= 0f)
         {
             Debug.Log("morreu");
         }
@@ -37,8 +48,6 @@ public class Health : MonoBehaviour
         {
             Damage(1);
             rb.AddForce(transform.up * 800);
-
-
         }
     }
 
@@ -46,14 +55,14 @@ public class Health : MonoBehaviour
     public void Damage(int GiveDamageAmount)
     {
         CameraShakeManager.instance.CameraShake(impulseSource);
-        lifeBar -= GiveDamageAmount;
-        lifeImages[lifeBar].gameObject.SetActive(false);
+        currentLife -= GiveDamageAmount;
+        lifeImages[currentLife].gameObject.SetActive(false);
     }
 
     //give heath
     public void GiveHealth(int GiveHealthAmount)
     {
-        lifeBar += GiveHealthAmount;
-        lifeImages[lifeBar].gameObject.SetActive(true);
+        currentLife += GiveHealthAmount;
+        lifeImages[currentLife].gameObject.SetActive(true);
     }
 }
