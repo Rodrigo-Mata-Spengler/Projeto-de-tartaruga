ec27eb72b7261bb7c7cf4bf25cb02b14c6e25515
Lozic71 <luizhenriquegm71@gmail.com>
2023-04-25 23:44:01 -0300

    puzzle

--- Assets/CircleButtomScript.cs
+++ Assets/CircleButtomScript.cs
@@ -7,6 +7,7 @@ public class CircleButtomScript : MonoBehaviour
     public bool PlayerDetected = false;
 
     [SerializeField] private Transform CircleToMove;
+    [SerializeField] private Transform CircleToMove1;
     [SerializeField] private float AmountToRotate;
     [Space]
     float speed = 2f;
@@ -15,6 +16,7 @@ public class CircleButtomScript : MonoBehaviour
     public bool rotates = false;
 
     public Transform TargetRotation;
+    public Transform TargetRotation1;
 
     private void Update()
     {
@@ -27,12 +29,22 @@ public class CircleButtomScript : MonoBehaviour
         if (rotates)
         {
             CircleToMove.rotation = Quaternion.SlerpUnclamped(CircleToMove.rotation, TargetRotation.rotation, 0.013f);
-            Debug.Log("Aquii");
+            if(CircleToMove1 != null)
+            {
+                CircleToMove1.rotation = Quaternion.SlerpUnclamped(CircleToMove1.rotation, TargetRotation1.rotation, 0.013f);
+            }
+            
+
             timeCount += Time.deltaTime;
 
             if(timeCount > 4)
             {
                 TargetRotation.Rotate(0f, 0f, AmountToRotate, Space.Self);
+                if(TargetRotation1 != null)
+                {
+                    TargetRotation1.Rotate(0f, 0f, AmountToRotate, Space.Self);
+                }
+                
                 timeCount = 0;
                 rotates = false;
             }
