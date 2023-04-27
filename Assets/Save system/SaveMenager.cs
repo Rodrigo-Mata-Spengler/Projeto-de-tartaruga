986e8bff965a5955de3ec6ef6683751cad55f9d9
Rodrigo Mata <rodrigomataspengler@gmail.com>
2023-04-27 12:04:29 -0300

    criando system maneger do menu

---
+++ Assets/Save system/SaveMenager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SaveMenager : MonoBehaviour
+{
+    //verifica se existe este save, caso não, deshabilita o botão
+    //ao clicar o botão faz o load do jogo
+
+}
