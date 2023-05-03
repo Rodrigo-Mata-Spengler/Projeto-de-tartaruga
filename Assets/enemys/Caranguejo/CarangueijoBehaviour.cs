d804a774a2357efca7224611410b863cc6ad7a0c
Rodrigo Mata <rodrigomataspengler@gmail.com>
2023-05-03 16:16:24 -0300

    Iniciando caranguejo

---
+++ Assets/enemys/Caranguejo/CarangueijoBehaviour.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum CarangueijoStatus {dormindo, caindo, espera, pulando};
+public class CarangueijoBehaviour : MonoBehaviour
+{
+    [Header("espera queda player")]
+    [SerializeField] public bool dectPlayer = false;
+
+    
+
+    //espera o player passar por baixo 
+    //caso o plyer passe por baixo ele se atira diretamente para baixo
+    //ao cair no chão ele acorda
+    //ele se atira em direçao ao player
+}
