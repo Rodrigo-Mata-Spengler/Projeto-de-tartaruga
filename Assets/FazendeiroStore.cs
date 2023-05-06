aa738292c7a434fae774382eb27d9bbbdf96abe5
Lozic71 <luizhenriquegm71@gmail.com>
2023-05-06 19:40:13 -0300

    bugs

--- Assets/FazendeiroStore.cs
+++ Assets/FazendeiroStore.cs
@@ -10,7 +10,7 @@ public class FazendeiroStore : MonoBehaviour
     private int Cost; // The Amount of the item (depending witch npc the item will be different) the player needs to be able to buy/trade
     public TextMeshProUGUI costText; // the text on the UI
 
-
+    public GameObject SelectedButton;
 
     private void Awake()
     {
