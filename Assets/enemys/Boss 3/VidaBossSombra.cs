1343bb2287d830da5ac6a23b42db4e642481813b
Rodrigo Mata <rodrigomataspengler@gmail.com>
2023-05-16 20:40:18 -0300

    update

--- Assets/enemys/Boss 3/VidaBossSombra.cs
+++ Assets/enemys/Boss 3/VidaBossSombra.cs
@@ -7,11 +7,6 @@ public class VidaBossSombra : MonoBehaviour
     [Header("Vida do Boss")]
     [SerializeField] public static float vidaAtual = 0;
 
-    private void Update()
-    {
-        Debug.Log(vidaAtual);
-    }
-
     public static void TomarDano(float Dano)
     {
         if (Dano-vidaAtual <= 0)
