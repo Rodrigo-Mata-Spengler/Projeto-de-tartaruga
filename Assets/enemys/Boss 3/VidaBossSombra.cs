f88683a30268974ce5f46dab3fba5d8bac13f0fb
Rodrigo Mata <rodrigomataspengler@gmail.com>
2023-05-16 20:39:11 -0300

    Vida Sombra e alguns ajustes

---
+++ Assets/enemys/Boss 3/VidaBossSombra.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VidaBossSombra : MonoBehaviour
+{
+    [Header("Vida do Boss")]
+    [SerializeField] public static float vidaAtual = 0;
+
+    private void Update()
+    {
+        Debug.Log(vidaAtual);
+    }
+
+    public static void TomarDano(float Dano)
+    {
+        if (Dano-vidaAtual <= 0)
+        {
+            Morreu();
+        }
+        else
+        {
+            vidaAtual = Dano - vidaAtual;
+        }
+    }
+
+    private static void Morreu()
+    {
+
+    }
+}
